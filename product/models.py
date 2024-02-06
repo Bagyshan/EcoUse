3dc362cdf6c56cbfc1c0ed80293f9d0518e6ef2f
Bagyshan <bagishan01@gmail.com>
2024-02-06 21:25:19 +0600

    ......

--- product/models.py
+++ product/models.py
@@ -22,6 +22,8 @@ class Product(models.Model):
     )
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now = True)
+    image = models.ImageField(upload_to='product_img/',null=True, blank=True)
+    price = models.PositiveIntegerField()
 
     def save(self, *args, **kwargs):
         self.updated_at = timezone.now().date()
