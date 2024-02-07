45def8c070866e7e980943b6f437ae99e98cbb10
Bagyshan <bagishan01@gmail.com>
2024-02-08 05:56:07 +0600

    ....

--- account/tasks.py
+++ account/tasks.py
@@ -2,7 +2,7 @@ from .send_email import send_confirmation_email
 from celery import shared_task
 from config.celery import app
 
-@app.task
+@shared_task
 def send_confirmation_email_task(email, code):
     send_confirmation_email(email, code)
 
