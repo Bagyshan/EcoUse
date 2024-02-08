d990d642d0772abec29735a80a1979fa5befd57f
Erlando007 <@Erlando007>
2024-02-08 20:06:59 +0600

    commit

--- account/send_email.py
+++ account/send_email.py
@@ -3,7 +3,7 @@ from django.core.mail import send_mail
 from django.utils.html import format_html
 
 def send_confirmation_email(email, code):
-    activation_url = f'http://localhost:8000/account/activate/?u={code}'
+    activation_url = f'http://localhost:3000/account/activate/?u={code}'
     message = format_html(
         'Здравствуйте, активируйте ваш аккаунт'
         '<br>'
@@ -29,7 +29,7 @@ def send_confirmation_email(email, code):
 
 
 def send_password_reset_email(email, user_id):
-    password_reset_url = f'http://localhost:8000/account/password_confirm/{user_id}'
+    password_reset_url = f'http://localhost:3000/account/password_confirm/{user_id}'
     message = format_html(
         'Здравствуйте, чтобы восстановить пароль, вам нужно перейти по ссылке:'
         '<br>'
